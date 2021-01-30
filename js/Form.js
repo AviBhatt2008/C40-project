2f94a46ad3d31d8dcf2ba51a30e28d381f8fc56c
AviBhatt2008 <73443433+AviBhatt2008@users.noreply.github.com>
2021-01-30 23:23:06 +0000

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,60 @@
+class Form
+{
+    constructor()
+    {
+        // create variables for buttons, inputs and text
+       this.input = createInput("Name");
+       this.button = createButton('Play');
+       this.greeting = createElement('h2');
+       this.title = createElement('h2');
+    }
+    hide() 
+    {
+        // hide everything
+        this.greeting.hide();
+        this.button.hide();
+        this.input.hide();
+        this.title.hide();
+    }
+    display()
+    {
+        // make title
+        this.title.html("FRUIT CATCHER");
+        this.title.position(350, 50);
+        this.title.style('font-size', '70px');
+        this.title.style('color', 'skyblue');
+        // make input
+        this.input.position(550,400);
+        this.input.style('width', '200px');
+        this.input.style('height', '20px');
+        this.input.style('background', 'lavender');
+        // make button
+        this.button.position(560,500);
+        this.button.style('width', '200px');
+        this.button.style('height', '40px');
+        this.button.style('background', 'lightpink');
+
+        // if the button is pressed
+        this.button.mousePressed(() => {
+            // hide input and button
+            this.input.hide();
+            this.button.hide();
+            // player.name becomes the value of their input
+            player.name = this.input.value();
+            // the player count increases
+            playerCount += 1;
+            // the index becomes the player count (1st player gets 1, e.t.c)
+            player.index = playerCount;
+            // make all categories in database for players
+            player.update();
+            // make the playerCount in database match the one in game
+            player.updateCount(playerCount);
+            // display a greeting
+            this.greeting.html("Hello " + player.name)
+            this.greeting.position(400,250);
+            this.greeting.style('color', 'white');
+            this.greeting.style('font-size', '100px');
+        });
+
+    }
+}
---
+++ js/Game.js
@@ -0,0 +1,133 @@
+class Game
+{
+    constructor()
+    {
+
+    }
+    // get the gameState
+    getState() {
+        var gameStateRef = database.ref('gameState');
+        gameStateRef.on("value", function (data) {
+            gameState = data.val();
+        })
+
+    }
+    // update gameState
+    update(state) {
+        database.ref('/').update({
+            gameState: state
+        });
+    }
+    // at the start of the game
+    async start()
+    {
+        // if the game is at the start but not play
+        if (gameState === 0)
+        {
+            // make object for player
+            player = new Player();
+            // refer to playerCount
+            var playerCountRef = await database.ref('playerCount').once("value");
+            if (playerCountRef.exists())
+            {
+                // get the playerCount value
+                playerCount = playerCountRef.val();
+                player.getCount();
+            }
+            // make form and display
+            form = new Form()
+            form.display();
+        }
+
+        // create player1 sprite and add image
+        player1 = createSprite(200,500);
+        player1.addImage("player1",player_img);
+        // create player2 sprite and add image
+        player2 = createSprite(800,500);
+        player2.addImage("player2", player_img);
+        // put sprites in array
+        players=[player1,player2];
+    }
+    
+    play(){
+        // hide the form
+        form.hide();
+
+        // get all the players information
+        Player.getPlayerInfo();
+        // make an image to fit the screen
+        image(back_img, 0, 0, 1000, 800);
+        // declare x and y
+        var x =0;
+        var y=0;
+        // declare index for for loop
+        var index =0;
+
+        // draw sprites
+        drawSprites();
+
+        // for each player
+        for(var plr in allPlayers)
+        {
+            // make index increase
+            index = index+1;
+
+            // make x position 500 - distance
+            x = 500-allPlayers[plr].distance;
+            // make y 500
+            y=500;
+            
+            // make player's x = x and player's y = y
+            players[index -1].x = x;
+            players[index - 1].y = y;
+
+            /* Differentiate the main player by printing 
+            the name of the player on the basket.*/
+            if(index === player.index)
+            {
+                fill("black");
+                textSize(25);
+                text(allPlayers[plr].name, x - 40, y+25);
+            }
+        }
+
+        // Give movements for the players using arrow keys
+        if(keyDown(RIGHT_ARROW))
+        {
+            player.distance -= 10;
+            player.update();
+        }
+        if(keyDown(LEFT_ARROW))
+        {
+            player.distance += 10;
+            player.update();
+        }
+
+        // Create and spawn fruits randomly
+        if(frameCount % 20 === 0)
+        {
+            fruits = createSprite(random(100, 1000), 0, 100, 100);
+            fruits.velocityY = 6;
+            var rand = Math.round(random(1, 5));
+            switch(rand)
+            {
+                case 1: fruits.addImage(fruit1_img);
+                break;
+                case 2: fruits.addImage(fruit2_img);
+                break;
+                case 3: fruits.addImage(fruit3_img);
+                break;
+                case 4: fruits.addImage(fruit4_img);
+                break;
+                case 5: fruits.addImage(fruit5_img);
+                break;
+            }
+            fruitGroup.add(fruits);
+        }
+    }
+
+    // log that the game ended
+    end(){
+       console.log("Game Ended");
+    }
+}
